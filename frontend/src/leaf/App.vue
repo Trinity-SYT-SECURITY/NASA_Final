8888e1de423e31049a43d6dd4f94956ac4d292b2
死老百姓 <96654161+Trinity-SYT-SECURITY@users.noreply.github.com>
2024-10-04 07:30:52 +0800

    Add files via upload

---
+++ frontend/src/leaf/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <WithStreamlitConnection v-slot="{ args }">
+    <MyComponent :data="args.data" />
+  </WithStreamlitConnection>
+</template>
+
+<script setup lang="ts">
+import MyComponent from "./MyComponent.vue";
+import WithStreamlitConnection from "../streamlit/WithStreamlitConnection.vue";
+</script>
+
+<style>
+body,
+html {
+  height: 100%;
+  margin: 0;
+}
+</style>
